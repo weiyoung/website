26e745bfa28659d8bd4bdb73b859c809ae1b72a2
Weiyoung Tan <wtan@bgcengineering.ca>
2021-01-17 10:59:42 -0800

    created blank react template

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Weilcome to my weibsite
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <code>Learn React</code>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
