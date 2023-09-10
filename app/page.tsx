4742951c6c1dacc690d60b141d4a01cf3e89941e
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-09-10 15:23:58 -0700

    added tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default async function Home() {
   const projects = await getProjects()
 
   return (
-    <div>
+    <div className="max-w-5xl mx-auto py-20">
+      <h1 className="text-6xl font-extrabold">Hello, I'm weiyoung</h1>
+      <p className="text-xl my-6 text-gray-400">Check out my projects!</p>
+      <h2 className="font-bold mt-12 text-gray-600">Projects</h2>
       {projects.map((project) => (
         <div key={project._id}>{project.name}</div>
       ))}
