64856d888a92a8de22bd58af60e4a5203d8c1c5f
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-09-15 23:13:07 -0700

    added cool gradient

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,13 @@ export default async function Home() {
 
   return (
     <div className="max-w-5xl mx-auto py-20">
-      <h1 className="text-6xl font-extrabold">Hello, I'm weiyoung</h1>
+      <h1 className="text-6xl font-extrabold">
+        Hello, I'm{" "}
+        <span className="bg-gradient-to-r from-teal-700 via-sky-800 to-blue-950">
+          weiyoung
+        </span>{" "}
+        👋
+      </h1>
       <p className="text-xl my-6 text-gray-400">Check out my projects!</p>
       <h2 className="font-bold mt-12 text-gray-600">Projects</h2>
       {projects.map((project) => (
