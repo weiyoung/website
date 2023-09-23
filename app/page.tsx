b3442f10d1e08e14d85df227008d689588a71a76
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-09-23 11:32:15 -0700

    added more styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <div className="max-w-5xl mx-auto py-20">
       <h1 className="text-6xl font-extrabold">
         Hello, I'm{" "}
-        <span className="bg-gradient-to-r from-teal-700 via-sky-800 to-blue-950">
+        <span className="bg-gradient-to-r from-teal-700 via-sky-800 to-blue-950 bg-clip-text text-transparent">
           weiyoung
         </span>{" "}
         👋
@@ -15,7 +15,14 @@ export default async function Home() {
       <p className="text-xl my-6 text-gray-400">Check out my projects!</p>
       <h2 className="font-bold mt-12 text-gray-600">Projects</h2>
       {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
+        <div
+          key={project._id}
+          className="my-4 border border-gray-800 rounded-lg"
+        >
+          <div className="m-2 font-extrabold bg-gradient-to-r from-teal-700 via-sky-800 to-blue-950 bg-clip-text text-transparent">
+            {project.name}
+          </div>
+        </div>
       ))}
     </div>
   )
