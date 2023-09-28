bdf5016a61b918c300fb9c8fe0b536d9f017e273
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-09-27 23:24:11 -0700

    added ability to add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { getProjects } from "@/sanity/sanity-utils"
+import Image from "next/image"
 
 export default async function Home() {
   const projects = await getProjects()
@@ -12,18 +13,32 @@ export default async function Home() {
         </span>{" "}
         👋
       </h1>
+
       <p className="text-xl my-6 text-gray-400">Check out my projects!</p>
+
       <h2 className="font-bold mt-12 text-gray-600">Projects</h2>
-      {projects.map((project) => (
-        <div
-          key={project._id}
-          className="my-4 border border-gray-800 rounded-lg"
-        >
-          <div className="m-2 font-extrabold bg-gradient-to-r from-teal-700 via-sky-800 to-blue-950 bg-clip-text text-transparent">
-            {project.name}
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map((project) => (
+          <div
+            key={project._id}
+            className="my-2 border border-gray-800 rounded-lg"
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={100}
+                className="object-cover"
+              />
+            )}
+            <div className="p-3 font-extrabold bg-gradient-to-r from-teal-700 via-sky-800 to-blue-950 bg-clip-text text-transparent">
+              {project.name}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
