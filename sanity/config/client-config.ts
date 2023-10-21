19d8a8f515b8041bb153e5a2e151157d95bf29f2
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-10-21 00:18:42 -0700

    refactored sanity-utils

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "h8ww2091",
+  dataset: "production",
+  apiVersion: "2023-08-02",
+}
+
+export default config
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,14 +1,9 @@
 import { createClient, groq } from "next-sanity"
 import schemas from "./schemas/index"
+import clientConfig from "./config/client-config"
 
 export async function getProjects(): Promise<Project[]> {
-  const client = createClient({
-    projectId: "h8ww2091",
-    dataset: "production",
-    apiVersion: "2023-08-02",
-  })
-
-  return client.fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type == "project"]{
         _id,
         _createdAt,
@@ -22,13 +17,7 @@ export async function getProjects(): Promise<Project[]> {
 }
 
 export async function getProject(slug: string): Promise<Project> {
-  const client = createClient({
-    projectId: "h8ww2091",
-    dataset: "production",
-    apiVersion: "2023-08-02",
-  })
-
-  return client.fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type == "project" && slug.current == $slug][0]{
         _id,
         _createdAt,
