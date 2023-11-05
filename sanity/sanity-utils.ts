94beab7ba0d59d6614a82f478751502476e71355
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-11-04 22:41:32 -0700

    added page schema and type

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { createClient, groq } from "next-sanity"
-import schemas from "./schemas/index"
+import { Project } from "@/types/Project"
 import clientConfig from "./config/client-config"
+import { Page } from "@/types/Page"
 
 export async function getProjects(): Promise<Project[]> {
   return createClient(clientConfig).fetch(
@@ -26,6 +27,30 @@ export async function getProject(slug: string): Promise<Project> {
         "image": image.asset->url,
         url,
         content,
+    }`,
+    { slug }
+  )
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current
+    }`
+  )
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+      content
     }`,
     { slug }
   )
