f0f3d3530d6f73351c46f99c10338d48cab28d34
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-11-13 17:23:54 -0800

    created navbar pages

---
+++ app/(site)/[slug]/page.tsx
@@ -0,0 +1,19 @@
+import { getPage } from "@/sanity/sanity-utils"
+import { PortableText } from "@portabletext/react"
+
+type Props = {
+  params: { slug: string }
+}
+
+export default async function Page({ params }: Props) {
+  const page = await getPage(params.slug)
+
+  return (
+    <div>
+      <h1>{page.title}</h1>
+      <div className="">
+        <PortableText value={page.content} />
+      </div>
+    </div>
+  )
+}
