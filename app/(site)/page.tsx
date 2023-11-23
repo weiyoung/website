17b803ba5aa6e5a516c146709b516d2bf3ce0518
weiyoung <43733599+weiyoung@users.noreply.github.com>
2023-11-22 23:12:43 -0800

    updated content and refactored styles

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,9 +11,18 @@ export default async function Home() {
         Hello, I&apos;m <span className="custom-gradient">weiyoung</span> 👋
       </h1>
 
-      <p className="my-10">Check out my projects!</p>
+      <p className="my-16">
+        BSc in Computer Science at The University of British Columbia in
+        Vancouver, Canada 🇨🇦 <br />
+        <br />
+        Previous SDE intern at Amazon, BlackBerry, Galvanize(now Diligent) and
+        more! <br />
+        <br />
+        I'm a passionate, all-rounded software developer 💻 with experience in
+        Frontend, Backend, and Mobile development!
+      </p>
 
-      <h2>Projects</h2>
+      <h2 className="mb-4">Projects</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {projects.map((project) => (
