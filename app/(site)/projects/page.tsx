7a6769fffd56130b20e0cfe616a2be623c5722da
weiyoung <43733599+weiyoung@users.noreply.github.com>
2024-01-16 16:44:14 -0800

    added about and projects pages

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -2,28 +2,11 @@ import { getProjects } from "@/sanity/sanity-utils"
 import Image from "next/image"
 import Link from "next/link"
 
-export default async function Home() {
+export default async function Project() {
   const projects = await getProjects()
 
   return (
     <>
-      <h1>
-        Hello, I&apos;m <span className="custom-gradient">weiyoung</span> 👋
-      </h1>
-
-      <div className="my-12">
-        <p>
-          BSc in Computer Science at The University of British Columbia in
-          Vancouver, Canada 🇨🇦 <br />
-          <br />
-          Previous SDE intern at Amazon, BlackBerry, Galvanize(now Diligent) and
-          more! <br />
-          <br />
-          I&apos;m a passionate, all-rounded software developer 💻 with
-          experience in Frontend, Backend, and Mobile development!
-        </p>
-      </div>
-
       <h2 className="mb-6">Projects</h2>
 
       <div className="items-grid">
