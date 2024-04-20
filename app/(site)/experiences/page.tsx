b63bd3d1e264cbd45b9dce0b6002d3a98ad622af
weiyoung <43733599+weiyoung@users.noreply.github.com>
2024-04-20 10:58:24 -0700

    displayed experience details

--- app/(site)/experiences/page.tsx
+++ app/(site)/experiences/page.tsx
@@ -1,4 +1,5 @@
 import { getExperiences } from "@/sanity/sanity-utils"
+import { PortableText } from "@portabletext/react"
 
 export default async function Experiences() {
   const experiences = await getExperiences()
@@ -7,10 +8,26 @@ export default async function Experiences() {
     <>
       <h2>Experiences</h2>
 
-      <div className="items-grid">
+      <div>
         {experiences.map((experience) => (
           <div key={experience._id} className="items-card">
             <div>{experience.company}</div>
+            <div>{experience.position}</div>
+            <div>{String(experience.start_date)}</div>
+            <div>{String(experience.end_date)}</div>
+            <div>{experience.location}</div>
+            <div className="my-10">
+              <PortableText value={experience.content} />
+            </div>
+            {experience.tags && (
+              <div className="flex justify-left gap-4">
+                {experience.tags.map((tag) => (
+                  <div className="items-card" key={tag}>
+                    {tag}
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         ))}
       </div>
