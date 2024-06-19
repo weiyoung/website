c2ad0d444b00cf81cc26044c4d0b8746c8629d50
weiyoung <43733599+weiyoung@users.noreply.github.com>
2024-06-19 11:08:56 -0700

    updated spacing

--- app/(site)/components/intro.tsx
+++ app/(site)/components/intro.tsx
@@ -1,14 +1,11 @@
 export default function Intro() {
   return (
-    <>
-      <p>
-        All-rounded <b>software developer</b> 💻 based in Vancouver, Canada 🇨🇦
-      </p>
-      <p>
-        Avid learner with a <b>growth mindset</b> 🌱, always looking to learn
-        new things 🤓
-      </p>
-      <p>I enjoy music 🎹, gym 💪, outdoors ⛰️, food 😋, and philosophy 🤓</p>
-    </>
+    <p className="leading-9">
+      All-rounded <b>software developer</b> 💻 based in Vancouver, Canada 🇨🇦
+      <br />
+      Avid learner with a <b>growth mindset</b> 🌱, always looking to learn new
+      things 🤓
+      <br />I enjoy music 🎹, gym 💪, outdoors ⛰️, food 😋, and philosophy 🤓
+    </p>
   )
 }
